00fbee77345d5fd6543be1951fa015c7ce64bc27
KamenevArtem <kamenevart94@gmail.com>
2022-10-29 15:43:56 +0300

    dload_EPIC_img_from_nasa changing variables name

--- dload_EPIC_img_from_nasa.py
+++ dload_EPIC_img_from_nasa.py
@@ -12,17 +12,16 @@ def parse_EPIC(access_token, script_path, im_path):
     }
     response = requests.get(api_url, params=payload)
     response.raise_for_status
-    res = response.json()
-    for pic_number, data in enumerate(res):
-        pic_date = data["identifier"]
+    response = response.json()
+    for pic_number, img_data in enumerate(response):
+        pic_date = img_data["identifier"]
         mean_index = int(len(pic_date)/2)+1
-        pic_date = pic_date[: mean_index]
-        pic_date = f"{pic_date[0:4]}-{pic_date[4:6]}-{pic_date[6:8]}"
-        pic_date = datetime.date.fromisoformat(pic_date)
-        pic_name = data["image"]
-        parsed_link = f"{pic_date.year}/{pic_date.month}/{pic_date.day}/png/{pic_name}.png"
-        url = url_template.format(parsed_link)
-        print(url)
+        pic_date_template = pic_date[: mean_index]
+        pic_date_template = f"{pic_date[0:4]}-{pic_date[4:6]}-{pic_date[6:8]}"
+        pic_date = datetime.date.fromisoformat(pic_date_template)
+        pic_name = img_data["image"]
+        link_construction = f"{pic_date.year}/{pic_date.month}/{pic_date.day}/png/{pic_name}.png"
+        url = url_template.format(link_construction)
         image_request = requests.get(url, params=payload)
         image_request.raise_for_status
         image_name = f"EPIC_{pic_number}.png"
