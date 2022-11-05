8cc353711b2ae1b7fac46e310c4c0d09e4e1ec3c
KamenevArtem <kamenevart94@gmail.com>
2022-11-05 23:52:02 +0300

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -3,16 +3,17 @@ import os
 from dotenv import load_dotenv
 
 
-def bot(api_token, bot_chat_id):
+def bot(api_token):
     bot = telegram.Bot(token = api_token)
+    bot_chat_id = bot.get_updates()[-1].message.chat_id
     bot.send_message(text='Hi devman', chat_id=bot_chat_id)
+    #bot.send_document(chat_id=bot_chat_id, document=)
 
 
 def main():
     load_dotenv()
     access_token = os.environ["telegram_api_key"]
-    bot_chat_id = os.environ["bot_chat_id"]
-    bot(access_token, bot_chat_id) 
+    bot(access_token) 
 
 
 if __name__ == "__main__":
