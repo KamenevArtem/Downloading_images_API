031cde6eb08c3f4aa5c683493435fad312de4f22
KamenevArtem <kamenevart94@gmail.com>
2022-11-18 17:27:24 +0300

    Splitting functions

--- telegram_bot.py
+++ telegram_bot.py
@@ -17,24 +17,28 @@ def parse_args_bot():
     return arg
 
 
-def get_file_names(path, dir):
-    file_names = [] 
+def get_file_paths(path, dir):
+    file_paths = [] 
     for adress, dirs, files in os.walk(os.path.join(path, dir)):
         for name in files:
-            file_names.append(name)
-    return file_names
+            file_paths.append(os.path.join(adress, name))
+    return(file_paths)
 
 
-def send_images(api_token, script_path, sleep_time, file_dir, tg_chat_id):
+def send_image(bot, file_path, tg_chat_id):
+    with open(file_path, 'rb') as posting_file:
+        bot.send_document(chat_id=tg_chat_id, 
+                        document=posting_file)
+
+
+def sending_images_bot(api_token, script_path, sleep_time, file_dir, tg_chat_id):
     bot = telegram.Bot(token = api_token)
-    files_name = get_file_names(script_path, file_dir)
+    file_paths = get_file_paths(script_path, file_dir)
     while True:
-        random.shuffle(files_name)
-        for file_name in files_name:
+        random.shuffle(file_paths)
+        for file_path in file_paths:
+            send_image(bot, file_path, tg_chat_id)
             time.sleep(sleep_time*60)
-            with open(os.path.join(script_path, file_dir, file_name), 'rb') as posting_file:
-                bot.send_document(chat_id=tg_chat_id, 
-                                document=posting_file)
 
 
 def main():
@@ -45,7 +49,7 @@ def main():
     script_path = pathlib.Path.cwd()
     access_token = os.environ["TG_API_KEY"]
     chat_id = os.environ["TG_CHAT_ID"]
-    send_images(access_token, script_path, sleep_time, file_dir, chat_id)
+    sending_images_bot(access_token, script_path, sleep_time, file_dir, chat_id)
 
 
 if __name__ == "__main__":
